5c46df9eb3f56bcc7b83d4c932c081a6ca23c00e
patricio-jp <patojimenezpaz@gmail.com>
2023-03-04 21:10:46 -0300

    Visual part done

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    fontFamily: {
+      sans: ["Plus Jakarta Sans", "sans-serif"]
+    },
+    extend: {},
+  },
+  plugins: [],
+}
